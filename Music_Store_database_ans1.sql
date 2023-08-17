8b23d304d9de8277daedbc18d3b90aa4d0578a71
srustihs <124963723+srustihs@users.noreply.github.com>
2023-08-17 09:45:47 +0530

    Add files via upload

---
+++ Music_Store_database_ans1.sql
@@ -0,0 +1,19 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# SQL Server Management Studio Solution File, Format Version 19.00
+VisualStudioVersion = 15.0.28307.421
+MinimumVisualStudioVersion = 10.0.40219.1
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Default|Default = Default|Default
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{389E7EF9-2204-42C1-A7FA-162D2B401A18}.Default|Default.ActiveCfg = Default
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {D9690303-4C48-4F36-AB90-9DB5CD61AEC2}
+	EndGlobalSection
+EndGlobal
